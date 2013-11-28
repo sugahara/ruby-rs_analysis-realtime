cf5f50baa6c169306a2a90670d6bc6d75aaaf09b
Jun Sugahara <j.sghr.jp@gmail.com>
2013-11-28 14:32:06 +0900

    add normal ts logger and change shift log name

--- main_shift.rb
+++ main_shift.rb
@@ -23,8 +23,9 @@ class Main2
     :m_c => 50,
     :use_delta_n => true
   }
-  hurst_change_logger = RSAnalysis::HurstLogger.new(STATISTICS_DIR+"/#{@file_name}.hurst")
+  hurst_change_logger = RSAnalysis::HurstLogger.new(STATISTICS_DIR+"/#{@file_name}.shift_hurst")
   hurst_change_logger.info("#Number MeanHurst MaxHurst MinHurst")
+  timeseries_logger = RSAnalysis::TimeseriesLogger.new(STATISTICS_DIR+"/#{@file_name}.ts")
   ts = Timeseries.new(900)
 
   data_index = 0
@@ -33,7 +34,8 @@ class Main2
     rate_value = str.split(" ")
     timestamp = rate_value[0]
     data = rate_value[1].to_f
-    ts.insert(data, timestamp) 
+    ts.insert(data, timestamp)
+    timeseries_logger.log_timeseries([timestamp], [data])
     if ts.size >= ts.window_size
       @data = ts.to_a[0]
       @timestamp = ts.to_a[1]
@@ -46,11 +48,11 @@ class Main2
       rs_result = rs.get_hurst()
       #rs.show_config
       if rs_result != RSAnalysis::Error::HURST_ERROR
-        statistics_logger = RSAnalysis::StatisticsLogger.new(STATISTICS_DIR+"/#{@file_name}#{data_index}.rsresult")
+        statistics_logger = RSAnalysis::StatisticsLogger.new(STATISTICS_DIR+"/#{@file_name}#{data_index}.shift_rsresult")
         statistics_logger.log_leastsquare_result(rs_result[2])
         statistics_logger.log_statistics(rs_result[1])
-        timeseries_logger = RSAnalysis::TimeseriesLogger.new(STATISTICS_DIR+"/#{@file_name}#{data_index}.ts")
-        timeseries_logger.log_timeseries(@timestamp,@data)
+        timeseries_shift_logger = RSAnalysis::TimeseriesLogger.new(STATISTICS_DIR+"/#{@file_name}#{data_index}.shift_ts")
+        timeseries_shift_logger.log_timeseries(@timestamp,@data)
       else
         puts rs_result
       end
@@ -60,7 +62,7 @@ class Main2
       hurst_logger.info("#{@start_time} #{data_index} #{rs_result[0][0]} #{rs_result[0][1]} #{rs_result[0][2]}")
 
       hurst_change_logger.info("#{@timestamp[0].to_f} #{data_index} #{rs_result[0][0]} #{rs_result[0][1]} #{rs_result[0][2]} #{@start_time}")
-      hurst_trans_logger = RSAnalysis::HurstLogger.new(STATISTICS_DIR+"/#{@file_name}#{data_index}.hursttrans")
+      hurst_trans_logger = RSAnalysis::HurstLogger.new(STATISTICS_DIR+"/#{@file_name}#{data_index}.shift_hursttrans")
       #Hurst transition log
       hurst_trans_logger.info("#Range MeanHurst MaxHurst MinHurst")
       rs_result[3].each_with_index do |hurst, i|
